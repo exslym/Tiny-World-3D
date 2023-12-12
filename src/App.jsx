9918fa89aa58c9c38ff1fa5e8ac3c6f61c0c9652
exslym <76844562+exslym@users.noreply.github.com>
2023-12-12 16:48:04 +0400

    pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './index.scss';
+import { About, Contact, Home, Projects } from './pages';
 
 export const App = () => {
 	return (
@@ -8,10 +9,10 @@ export const App = () => {
 			<Router>
 				<Navbar />
 				<Routes>
-					<Route path='/' element={'Home'} />
-					<Route path='/about' element={'About'} />
-					<Route path='/projects' element={'Projects'} />
-					<Route path='/contact' element={'Contact'} />
+					<Route path='/' element={<Home />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/projects' element={<Projects />} />
+					<Route path='/contact' element={<Contact />} />
 				</Routes>
 			</Router>
 		</main>
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+import Projects from './Projects';
+
+export { About, Contact, Home, Projects };
