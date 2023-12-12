a7bf3c36a5d7e8230c9fd696c2c8084c0437994a
exslym <76844562+exslym@users.noreply.github.com>
2023-12-12 16:18:32 +0400

    project started

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import react from '@vitejs/plugin-react';
+import Path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import { ViteAliases } from 'vite-aliases';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = Path.dirname(__filename);
+
+export default defineConfig({
+	root: Path.resolve(__dirname, './src'),
+	publicDir: '../public',
+	build: {
+		emptyOutDir: true,
+	},
+	css: {
+		devSourcemap: true,
+	},
+	plugins: [react(), ViteAliases()],
+	server: {
+		hmr: true,
+		port: 3000,
+		host: '0.0.0.0',
+	},
+});
