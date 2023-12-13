4f38e116e5b7668a93bec39f41b40127ba94bda4
exslym <76844562+exslym@users.noreply.github.com>
2023-12-13 10:16:44 +0400

    project renamed

--- vite.config.js
+++ vite.config.js
@@ -23,39 +23,3 @@ export default defineConfig({
 		host: '0.0.0.0',
 	},
 });
-
-// const pickedKeys = [
-// 	'VITE_APP_EMAILJS_SERVICE_ID',
-// 	'VITE_APP_EMAILJS_TEMPLATE_ID',
-// 	'VITE_APP_EMAILJS_PUBLIC_KEY',
-// 	'VITE_APP_EMAILJS_TO_EMAIL',
-// 	'VITE_APP_EMAILJS_TO_NAME',
-// ];
-
-// export default defineConfig(({ mode }) => {
-// 	const process = Node.Process;
-// 	const env = loadEnv(mode, process.cwd(), '');
-// 	const processEnv = {};
-// 	pickedKeys.forEach(key => (processEnv[key] = env[key]));
-
-// 	return {
-// 		define: {
-// 			'process.env': processEnv,
-// 		},
-// 		root: Path.resolve(__dirname, './src'),
-// 		publicDir: '../public',
-// 		build: {
-// 			emptyOutDir: true,
-// 		},
-// 		css: {
-// 			devSourcemap: true,
-// 		},
-// 		plugins: [react(), ViteAliases()],
-// 		assetsInclude: ['**/*.glb'],
-// 		server: {
-// 			hmr: true,
-// 			port: 3000,
-// 			host: '0.0.0.0',
-// 		},
-// 	};
-// });
