764099b6067738b7eb09fcea9cd43b7846b44873
exslym <76844562+exslym@users.noreply.github.com>
2023-12-14 20:18:55 +0400

    Loader position fixed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import { Html } from '@react-three/drei';
 
 const Loader = () => {
 	return (
-		<Html>
+		<Html style={{ transform: 'translate(-50%, 0)' }}>
 			<div className='flex justify-center items-center'>
 				<div className='w-20 h-20 border-2 border-opacity-50 border-slate-300 border-t-blue-500 rounded-full animate-spin' />
 			</div>
